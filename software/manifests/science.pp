e3dff0e7afeb9c72168958eb14f72ee3d291c677
vit01 <me@ii-net.tk>
2020-12-08 13:39:02 +0800

    add path to science config

--- software/manifests/science.pp
+++ software/manifests/science.pp
@@ -28,7 +28,10 @@ class software::science {
 	package { $pkgs_uninst: ensure => "absent" }
     
     package { $pip_packages: ensure => 'latest', provider => 'pip3' }
-    package { 'ipywidgets': ensure => 'latest', provider => 'pip3' } -> exec { 'enable jupyter extension': command => '/usr/local/bin/jupyter nbextension enable --py widgetsnbextension' } -> exec { 'install jupyterlab extension': command => '/usr/local/bin/jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build' } -> exec {'jupyter lab build without minimize': command => '/usr/local/bin/jupyter lab build --minimize=False'}
     
-    package { 'dot_kernel': ensure => 'latest', provider => 'pip3' } -> exec { 'install dot kernel': command  => '/usr/local/bin/install-dot-kernel' } -> exec { 'install for user': command  => '/usr/local/bin/install-dot-kernel', user => $user }
+    $all_path = '/usr/local/bin/:/usr/bin'
+    
+    package { 'ipywidgets': ensure => 'latest', provider => 'pip3' } -> exec { 'enable jupyter extension': path => $all_path, command => 'jupyter nbextension enable --py widgetsnbextension' } -> exec { 'install jupyterlab extension': path => $all_path, command => 'jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build' } -> exec {'jupyter lab build without minimize': path => $all_path, command => 'jupyter lab build --minimize=False'}
+    
+    package { 'dot_kernel': ensure => 'latest', provider => 'pip3' } -> exec { 'install dot kernel': path => $all_path, command  => 'install-dot-kernel' } -> exec { 'install for user': path => $all_path, command  => 'install-dot-kernel', user => $user }
 }
