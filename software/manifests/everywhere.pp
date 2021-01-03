63f1ca176479f6950107d25d25f576452ae52010
vit01 <me@ii-net.tk>
2021-01-03 20:53:43 +0800

    remove yaourt and add yay

--- software/manifests/everywhere.pp
+++ software/manifests/everywhere.pp
@@ -20,9 +20,9 @@ class software::everywhere {
 
 	$pkgs_common = ['vim', 'git', 'curl', 'bash-completion', 'busybox', 'diffutils', 'dkms', 'elinks', 'file', 'findutils', 'gdb', 'graphicsmagick', 'grep', 'moreutils', 'ncdu', 'nmap', 'p7zip', 'parallel', 'patch', 'pciutils', 'rsync', 'screen', 'sed', 'sshfs', 'sudo', 'unzip', 'wget', 'wireguard-dkms', 'wireguard-tools', 'ethtool', 'fdupes', 'iftop', 'iotop', 'lshw', 'lsof', 'zsh', 'unrar']
 	$pkgs_deb = ['adduser', 'apt-utils', 'apt-transport-https', 'build-essential', 'cron', 'openssh-client', 'openssh-server', 'openssh-sftp-server', 'python3-pip', 'wireguard', 'xz-utils', 'apt-file', 'wcalc']
-	$pkgs_arch = ['base-devel', 'cronie', 'openssh', 'python-pip', 'xz', 'calc', 'yaourt']
+	$pkgs_arch = ['base-devel', 'cronie', 'openssh', 'python-pip', 'xz', 'calc', 'yay']
 
-	$pkgs_uninst = []
+	$pkgs_uninst = ['yaourt']
 
 	$pkgs_custom = $operatingsystem ? {
 		debian => $pkgs_deb,
