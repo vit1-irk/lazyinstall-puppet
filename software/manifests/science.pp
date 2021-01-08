4ea94d68a1a281c7b0579b1f28632be85ade51a6
vit01 <me@ii-net.tk>
2021-01-09 02:57:39 +0800

    add gdl_kernel

--- software/manifests/science.pp
+++ software/manifests/science.pp
@@ -10,7 +10,7 @@ class software::science {
     
 	$pkgs_arch = ['r', 'ds9', 'texlive-bin', 'texlive-core', 'texlive-bibtexextra', 'texlive-fontsextra', 'texlive-latexextra', 'texlive-formatsextra', 'texlive-langcyrillic', 'texlive-langgreek']
     
-    $pip_packages = ['jupyter', 'jupyterlab', 'aiohttp', 'lxml', 'matplotlib', 'numpy', 'scipy', 'pandas', 'seaborn', 'pillow', 'astropy', 'sunpy', 'apprise', 'requests', 'bs4', 'drms', 'zeep', 'h5netcdf', 'ipywidgets', 'dot_kernel']
+    $pip_packages = ['jupyter', 'jupyterlab', 'aiohttp', 'lxml', 'matplotlib', 'numpy', 'scipy', 'pandas', 'seaborn', 'pillow', 'astropy', 'sunpy', 'apprise', 'requests', 'bs4', 'drms', 'zeep', 'h5netcdf', 'ipywidgets', 'dot_kernel', 'git+https://github.com/gnudatalanguage/gdl_kernel']
     
     $pkgs_uninst = []
 
