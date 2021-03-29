cf02cf27b9085449164c2ca45c26211d06d0b977
vit01 <me@ii-net.tk>
2021-03-29 13:52:31 +0800

    some server packages

--- software/manifests/server.pp
+++ software/manifests/server.pp
@@ -1,5 +1,5 @@
 class software::server {
-	$pkgs = ['letsencrypt', 'certbot', 'nginx', 'fail2ban', 'emacs-nox', 'tcpdump']
+	$pkgs = ['letsencrypt', 'certbot', 'python3-certbot-nginx', 'nginx', 'fail2ban', 'emacs-nox', 'tcpdump']
 	$pkgs_uninst = ['apache2-bin', 'apache2-utils']
     
 	package { $pkgs: ensure => "installed" }
