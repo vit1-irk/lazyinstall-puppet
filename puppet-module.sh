5b5d47810c6943164d69a31c2fb9291e281909a6
vit01 <me@ii-net.tk>
2021-08-18 17:37:19 +0800

    usb device rescanning workaround for asus m3a

--- puppet-module.sh
+++ puppet-module.sh
@@ -7,5 +7,4 @@ fi
 puppet module install saz-timezone --version 6.1.0
 puppet module install saz-locales --version 2.5.1
 puppet module install puppetlabs-vcsrepo --version 3.1.1
-#puppet module install camptocamp-systemd --version 2.10.0
 #puppet module install eyp-python --version 0.1.13
--- software/manifests/everywhere.pp
+++ software/manifests/everywhere.pp
@@ -7,7 +7,7 @@ class software::everywhere {
 
     # non-free repo is needed to install broadcom wifi drivers and unrar
     case $facts['os']['name'] { 'Debian': { 
-	if $facts['dmi']['manufacturer'] != 'Hetzner' {
+    if $facts['dmi']['manufacturer'] != 'Hetzner' {
         if $facts['os']['distro']['codename'] == 'buster' {
             file { '/etc/apt/sources.list.d/mybackport.list':
                 ensure => present,
@@ -19,26 +19,40 @@ class software::everywhere {
         include software::archlinuxcn
     }}
 
-	$pkgs_common = ['vim', 'git', 'curl', 'bash-completion', 'busybox', 'diffutils', 'dkms', 'elinks', 'file', 'findutils', 'gdb', 'graphicsmagick', 'grep', 'moreutils', 'ncdu', 'nmap', 'p7zip', 'parallel', 'patch', 'pciutils', 'rsync', 'screen', 'sed', 'sshfs', 'sudo', 'unzip', 'wget', 'wireguard-dkms', 'wireguard-tools', 'ethtool', 'fdupes', 'iftop', 'iotop', 'lshw', 'lsof', 'zsh', 'unrar', 'qrencode']
-	$pkgs_deb = ['adduser', 'apt-utils', 'apt-transport-https', 'build-essential', 'cron', 'openssh-client', 'openssh-server', 'openssh-sftp-server', 'python3-pip', 'wireguard', 'xz-utils', 'apt-file', 'wcalc', 'resolvconf']
-	$pkgs_arch = ['base-devel', 'cronie', 'openssh', 'python-pip', 'xz', 'calc', 'yay']
+    $pkgs_common = ['vim', 'git', 'curl', 'bash-completion', 'busybox', 'diffutils', 'dkms', 'elinks', 'file', 'findutils', 'gdb', 'graphicsmagick', 'grep', 'moreutils', 'ncdu', 'nmap', 'p7zip', 'parallel', 'patch', 'pciutils', 'rsync', 'screen', 'sed', 'sshfs', 'sudo', 'unzip', 'wget', 'wireguard-dkms', 'wireguard-tools', 'ethtool', 'fdupes', 'iftop', 'iotop', 'lshw', 'lsof', 'zsh', 'unrar', 'qrencode']
+    $pkgs_deb = ['adduser', 'apt-utils', 'apt-transport-https', 'build-essential', 'cron', 'openssh-client', 'openssh-server', 'openssh-sftp-server', 'python3-pip', 'wireguard', 'xz-utils', 'apt-file', 'wcalc', 'resolvconf']
+    $pkgs_arch = ['base-devel', 'cronie', 'openssh', 'python-pip', 'xz', 'calc', 'yay']
 
-	$pkgs_uninst = ['yaourt']
+    $pkgs_uninst = ['yaourt']
 
-	$pkgs_custom = $operatingsystem ? {
-		debian => $pkgs_deb,
-		ubuntu => $pkgs_deb,
-		archlinux => $pkgs_arch,
-		manjarolinux => $pkgs_arch
-	}
+    $pkgs_custom = $operatingsystem ? {
+        debian => $pkgs_deb,
+        ubuntu => $pkgs_deb,
+        archlinux => $pkgs_arch,
+        manjarolinux => $pkgs_arch
+    }
 
-	package { $pkgs_common: ensure => "present" }
-	package { $pkgs_custom: ensure => "present" }
-	package { $pkgs_uninst: ensure => "absent" }
+    package { $pkgs_common: ensure => "present" }
+    package { $pkgs_custom: ensure => "present" }
+    package { $pkgs_uninst: ensure => "absent" }
     
     file { '/usr/bin/easy-wg-quick':
         ensure => present,
         mode => "0755",
         source => 'https://raw.githubusercontent.com/burghardt/easy-wg-quick/master/easy-wg-quick'
+    }
+
+    if $facts['dmi']['board']['product'] == "M3A" {
+        file { '/etc/systemd/system/m3a-usbfix.service':
+            ensure => present,
+            mode => "0644",
+            content => file('software/m3a-usbfix.service')
+        }
+        service { 'm3a-usbfix':
+            name => "m3a-usbfix",
+            ensure => "running",
+            enable => "true",
+            require => File['/etc/systemd/system/m3a-usbfix.service']
+        }
     }
 }
