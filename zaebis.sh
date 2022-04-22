af1ead9ae20da84252a8d7ebdbe22b715d9322ac
Viktor (vit01) <vit1-irk@users.noreply.github.com>
2022-04-22 14:18:54 +0800

    Update zaebis.sh

--- zaebis.sh
+++ zaebis.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 if [ $UID != 0 ]; then
-	echo "you need to be root"
-	exit
+        echo "you need to be root"
+        exit
 fi
 
 
-if [ -n "`which apt-get`" ]; then apt-get -y install git puppet;
+if [ -n "`which apt-get`" ]; then
+        apt-get -y install git
+        apt-get -y purge puppet
+        source /etc/os-release
+        if [ "$VERSION_CODENAME" = "jammy" ]; then
+                VERSION_CODENAME="focal";
+        fi
+        wget https://apt.puppet.com/puppet7-release-$VERSION_CODENAME.deb -O puppet7-release-$VERSION_CODENAME.deb
+        dpkg -i puppet7-release-$VERSION_CODENAME.deb;
+        apt-get update
+        apt-get -y install puppet-agent
+        export PATH=$PATH:/opt/puppetlabs/bin
 elif [ -n "`which pacman`" ]; then pacman -Sy --needed --noconfirm git puppet; fi
 
 if git rev-parse --git-dir > /dev/null 2>&1; then
@@ -26,13 +37,13 @@ fi
 ./puppet-module.sh
 
 if [ "$1" = "desktop" ]; then
-	puppet apply --modulepath=".:/etc/puppetlabs/code/modules:/etc/puppet/code/modules" desktop.pp
+        puppet apply --modulepath=".:/etc/puppetlabs/code/modules:/etc/puppet/code/modules" desktop.pp
 fi
 
 if [ "$1" = "server" ]; then
-	puppet apply --modulepath=".:/etc/puppetlabs/code/modules:/etc/puppet/code/modules" server.pp
+        puppet apply --modulepath=".:/etc/puppetlabs/code/modules:/etc/puppet/code/modules" server.pp
 fi
 
 if [ "$1" = "science" ]; then
-	puppet apply --modulepath=".:/etc/puppetlabs/code/modules:/etc/puppet/code/modules" science.pp
+        puppet apply --modulepath=".:/etc/puppetlabs/code/modules:/etc/puppet/code/modules" science.pp
 fi
