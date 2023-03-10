1aa6813dec38f5c96719bf375228641ed1c8d70e
vit01 <me@ii-net.tk>
2023-03-10 17:05:30 +0800

    add latest platform tools for android and fdroidcl

--- puppet-module.sh
+++ puppet-module.sh
@@ -11,5 +11,6 @@ puppet module install puppetlabs-docker --version 4.4.0
 puppet module install saz-resolv_conf --version 5.0.0
 puppet module install puppet-openssl --version 2.0.1
 puppet module install saz-ssh --version 6.2.0
-puppet module install puppet-php --version 8.1.1
+puppet module install puppet-php --version 8.2.0
+puppet module install puppet-archive --version 6.1.1
 #puppet module install eyp-python --version 0.1.13
--- software/manifests/desktop.pp
+++ software/manifests/desktop.pp
@@ -3,9 +3,9 @@ class software::desktop {
 
     $pkgs_common = ['bleachbit', 'davfs2', 'ntfs-3g', 'qbittorrent', 'dosfstools', 'exfatprogs', 'gucharmap', 'gvfs', 'kde-cli-tools', 'kdeconnect', 'geany', 'modemmanager', 'mpv', 'os-prober', 'parted', 'gparted', 'pavucontrol', 'fatresize', 'pcmanfm-qt', 'udisks2', 'usbutils', 'telegram-desktop', 'yt-dlp', 'zenity', 'zip', 'squashfs-tools', 'syncthing', 'keepassxc', 'debootstrap', 'okular', 'f2fs-tools', 'gimp', 'remmina', 'mtpaint', 'powertop', 'refind', 'shadowsocks-libev', 'simplescreenrecorder', 'seahorse', 'thunderbird', 'tor', 'torsocks', 'translate-shell', 'onboard', 'qt5ct', 'materia-gtk-theme', 'papirus-icon-theme', 'grsync']
 
-    $pkgs_debbased = ['fdisk', 'gdisk', 'gvfs-fuse', 'wpasupplicant', 'x11-utils', 'xinit', 'nextcloud-desktop', 'android-tools-adb', 'bluez-tools', 'go-mtpfs', 'gstreamer1.0-plugins-base', 'gstreamer1.0-plugins-good', 'gstreamer1.0-pulseaudio', 'libgstreamer-plugins-bad1.0-0', 'proxychains', 'localepurge', 'libfsapfs1', 'libfsapfs-utils', 'broadcom-sta-dkms', 'fonts-firacode', 'x2goclient']
+    $pkgs_debbased = ['fdisk', 'gdisk', 'gvfs-fuse', 'wpasupplicant', 'x11-utils', 'xinit', 'nextcloud-desktop', 'bluez-tools', 'go-mtpfs', 'gstreamer1.0-plugins-base', 'gstreamer1.0-plugins-good', 'gstreamer1.0-pulseaudio', 'libgstreamer-plugins-bad1.0-0', 'proxychains', 'localepurge', 'libfsapfs1', 'libfsapfs-utils', 'broadcom-sta-dkms', 'fonts-firacode', 'x2goclient']
 
-    $pkgs_arch = ['firefox', 'firefox-i18n-ru', 'arch-install-scripts', 'wpa_supplicant', 'xorg-xinit', 'nextcloud-client', 'android-tools', 'bluez-utils', 'gst-libav', 'gst-plugins-bad', 'gst-plugins-base', 'gst-plugins-good', 'gst-plugins-ugly', 'gstreamer-vaapi', 'gvfs-mtp', 'broadcom-wl-dkms', 'discord', 'mtpfs', 'pacgraph', 'proxychains-ng', 'refind-drivers', 'thunderbird-i18n-ru', 'mypaint', 'ttf-fira-sans', 'ttf-fira-mono', 'ttf-fira-code']
+    $pkgs_arch = ['firefox', 'firefox-i18n-ru', 'arch-install-scripts', 'wpa_supplicant', 'xorg-xinit', 'nextcloud-client', 'bluez-utils', 'gst-libav', 'gst-plugins-bad', 'gst-plugins-base', 'gst-plugins-good', 'gst-plugins-ugly', 'gstreamer-vaapi', 'gvfs-mtp', 'broadcom-wl-dkms', 'discord', 'mtpfs', 'pacgraph', 'proxychains-ng', 'refind-drivers', 'thunderbird-i18n-ru', 'mypaint', 'ttf-fira-sans', 'ttf-fira-mono', 'ttf-fira-code']
     
     $pkgs_debian = ['bluez-firmware', 'firefox-esr', 'firefox-esr-l10n-ru', 'thunderbird-l10n-ru']
     $pkgs_ubuntu = ['firefox', 'firefox-locale-ru', 'thunderbird-locale-ru']
@@ -85,4 +85,22 @@ class software::desktop {
         }
     }
 
+    archive { "/tmp/android-platform-tools.zip":
+      source        => 'https://dl.google.com/android/repository/platform-tools-latest-linux.zip',
+      extract       => true,
+      extract_path  => '/opt',
+      creates       => '/opt/platform-tools',
+      cleanup       => true,
+    } -> file { '/usr/bin/adb':
+        ensure => 'link',
+        target => '/opt/platform-tools/adb',
+        force  => true,
+        mode => "0755"
+    }
+
+    file { '/usr/bin/fdroidcl':
+        ensure => present,
+        mode => "0755",
+        source => 'https://github.com/mvdan/fdroidcl/releases/download/v0.7.0/fdroidcl_v0.7.0_linux_amd64'
+    }
 }
